5ee635c6df7f84ae3b34cfcfc1ebe6aac3bdafb2
Daniel H <operas.referee.0e@icloud.com>
2023-12-29 08:15:01 +1030

    ...

--- 1_03(under-review)/source/render/render.c
+++ 1_03(under-review)/source/render/render.c
@@ -6,7 +6,7 @@
 /*   By: dhadding <operas.referee.0e@icloud.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/18 11:53:38 by dhadding          #+#    #+#             */
-/*   Updated: 2023/12/27 09:38:48 by dhadding         ###   ########.fr       */
+/*   Updated: 2023/12/29 08:14:49 by dhadding         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,16 +20,12 @@ t_v8	scene(t_program *program)
 	mlx_loop(program->view->mlx);
 }
 
+
 t_s32	render_scene(t_v8 *param)
 {
 	t_program	*program;
 
 	program = (t_program *)param;
 	
-	draw_pixel(312, 512, program, rgb_2_hex(program->amb_light->rgb, 0.2));
-	draw_pixel(412, 512, program, rgb_2_hex(program->amb_light->rgb, 0.4));
-	draw_pixel(512, 512, program, rgb_2_hex(program->amb_light->rgb, 0.6));
-	draw_pixel(612, 512, program, rgb_2_hex(program->amb_light->rgb, 0.8));
-	draw_pixel(712, 512, program, rgb_2_hex(program->amb_light->rgb, 1.0));
 	return (0);
 }
